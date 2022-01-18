6f408a84f834813647d6d398995fd73fcad7c1ba
garyb6 <gary.bennett6@outlook.com>
2022-01-18 16:33:30 +0000

    get by id now works

---
+++ client/src/components/SightingsGrid.js
@@ -0,0 +1,17 @@
+import SightingCard from './SightingCard';
+
+
+const SightingsGrid = ({sightings, removeSighting}) => {
+    const sightingsList = sightings.map((sighting) =>{
+        return <SightingCard sighting={sighting} key={sighting._id} removeSighting={removeSighting} />
+    });
+    
+    return (
+        <>
+            {sightingsList}
+        </>
+    );
+
+}
+
+export default SightingsGrid;
